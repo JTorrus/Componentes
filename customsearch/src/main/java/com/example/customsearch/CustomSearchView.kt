c605be48c3c2f49df3f9b85b901bfb631d060014
JTorrus <jtorrusgalindo@gmail.com>
2018-02-11 03:35:03 +0100

    Logic implemented

--- customsearch/src/main/java/com/example/customsearch/CustomSearchView.kt
+++ customsearch/src/main/java/com/example/customsearch/CustomSearchView.kt
@@ -2,92 +2,15 @@ package com.example.customsearch
 
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.drawable.Drawable
-import android.text.TextPaint
+import android.text.Layout
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.custom_search_view.view.*
 
-/**
- * TODO: document your custom view class.
- */
 class CustomSearchView : View {
-    private var mExampleString: String? = null // TODO: use a default from R.string...
-    private var mExampleColor = Color.RED // TODO: use a default from R.color...
-    private var mExampleDimension = 0f // TODO: use a default from R.dimen...
-    /**
-     * Gets the example drawable attribute value.
-     *
-     * @return The example drawable attribute value.
-     */
-    /**
-     * Sets the view's example drawable attribute value. In the example view, this drawable is
-     * drawn above the text.
-     *
-     * @param exampleDrawable The example drawable attribute value to use.
-     */
-    var exampleDrawable: Drawable? = null
-
-    private var mTextPaint: TextPaint? = null
-    private var mTextWidth: Float = 0.toFloat()
-    private var mTextHeight: Float = 0.toFloat()
-
-    /**
-     * Gets the example string attribute value.
-     *
-     * @return The example string attribute value.
-     */
-    /**
-     * Sets the view's example string attribute value. In the example view, this string
-     * is the text to draw.
-     *
-     * @param exampleString The example string attribute value to use.
-     */
-    var exampleString: String?
-        get() = mExampleString
-        set(exampleString) {
-            mExampleString = exampleString
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * Gets the example color attribute value.
-     *
-     * @return The example color attribute value.
-     */
-    /**
-     * Sets the view's example color attribute value. In the example view, this color
-     * is the font color.
-     *
-     * @param exampleColor The example color attribute value to use.
-     */
-    var exampleColor: Int
-        get() = mExampleColor
-        set(exampleColor) {
-            mExampleColor = exampleColor
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * Gets the example dimension attribute value.
-     *
-     * @return The example dimension attribute value.
-     */
-    /**
-     * Sets the view's example dimension attribute value. In the example view, this dimension
-     * is the font size.
-     *
-     * @param exampleDimension The example dimension attribute value to use.
-     */
-    var exampleDimension: Float
-        get() = mExampleDimension
-        set(exampleDimension) {
-            mExampleDimension = exampleDimension
-            invalidateTextPaintAndMeasurements()
-        }
-
     constructor(context: Context) : super(context) {
         init(null, 0)
     }
@@ -101,71 +24,35 @@ class CustomSearchView : View {
     }
 
     private fun init(attrs: AttributeSet?, defStyle: Int) {
-        // Load attributes
-        val a = context.obtainStyledAttributes(
-                attrs, R.styleable.CustomSearchView, defStyle, 0)
+        val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-        mExampleString = a.getString(
-                R.styleable.CustomSearchView_exampleString)
-        mExampleColor = a.getColor(
-                R.styleable.CustomSearchView_exampleColor,
-                mExampleColor)
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mExampleDimension = a.getDimension(
-                R.styleable.CustomSearchView_exampleDimension,
-                mExampleDimension)
+        inflater.inflate(R.layout.custom_search_view, null)
 
-        if (a.hasValue(R.styleable.CustomSearchView_exampleDrawable)) {
-            exampleDrawable = a.getDrawable(
-                    R.styleable.CustomSearchView_exampleDrawable)
-            exampleDrawable!!.callback = this
+        val typedArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomSearchView, defStyle, 0)
+        val charSequence: CharSequence = typedArray.getString(R.styleable.CustomSearchView_hintText)
+
+        if (charSequence.isNotEmpty()) {
+            setHintText(typedArray.toString())
         }
 
-        a.recycle()
+        val textSize: Int = typedArray.getDimensionPixelOffset(R.styleable.CustomSearchView_textSize, 16)
 
-        // Set up a default TextPaint object
-        mTextPaint = TextPaint()
-        mTextPaint!!.flags = Paint.ANTI_ALIAS_FLAG
-        mTextPaint!!.textAlign = Paint.Align.LEFT
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements()
-    }
-
-    private fun invalidateTextPaintAndMeasurements() {
-        mTextPaint!!.textSize = mExampleDimension
-        mTextPaint!!.color = mExampleColor
-        mTextWidth = mTextPaint!!.measureText(mExampleString)
-
-        val fontMetrics = mTextPaint!!.fontMetrics
-        mTextHeight = fontMetrics.bottom
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        val paddingLeft = paddingLeft
-        val paddingTop = paddingTop
-        val paddingRight = paddingRight
-        val paddingBottom = paddingBottom
-
-        val contentWidth = width - paddingLeft - paddingRight
-        val contentHeight = height - paddingTop - paddingBottom
-
-        // Draw the text.
-        canvas.drawText(mExampleString!!,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint!!)
-
-        // Draw the example drawable on top of the text.
-        if (exampleDrawable != null) {
-            exampleDrawable!!.setBounds(paddingLeft, paddingTop,
-                    paddingLeft + contentWidth, paddingTop + contentHeight)
-            exampleDrawable!!.draw(canvas)
+        if (textSize > 0) {
+            setTextSize(textSize.toFloat())
         }
+
+        typedArray.recycle()
+    }
+
+    fun setHintText(text: String) {
+        searchText.hint = text
+        invalidate()
+        requestLayout()
+    }
+
+    fun setTextSize(size: Float) {
+        searchText.textSize = size
+        invalidate()
+        requestLayout()
     }
 }
