3977fe1ee81f3885bf4642deb7fa27a1aa3dca22
JTorrus <jtorrusgalindo@gmail.com>
2018-02-11 03:05:25 +0100

    init project

---
+++ app/src/main/java/com/example/javier_t/components_jtorrus/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.javier_t.components_jtorrus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
